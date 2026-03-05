39ce04517cdb86723276cd8c676a71d9db824333
AirDDD <airccc@outlook.com>
2026-03-05 15:49:56 +0800

    Create map_page.dart

---
+++ lib/pages/map_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MapPage extends StatelessWidget {
+  const MapPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        "地图（未来会显示家庭成员位置）",
+        style: TextStyle(fontSize: 18),
+      ),
+    );
+  }
+}
