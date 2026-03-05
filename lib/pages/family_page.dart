9e07898ac933f62484204a928087f9df45d1a66f
AirDDD <airccc@outlook.com>
2026-03-05 15:50:09 +0800

    Create family_page.dart

---
+++ lib/pages/family_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FamilyPage extends StatelessWidget {
+  const FamilyPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        "家庭（未来会有加入家庭 / 扫码 / 成员列表）",
+        style: TextStyle(fontSize: 18),
+      ),
+    );
+  }
+}
